f75f9269b93dfa390d62f2790c02d8e669d3244c
mohammedayman777 <ma75073377@gmail.com>
2025-11-03 00:42:38 +0200

    PlayerGUI.h

--- PlayerGUI.h
+++ PlayerGUI.h
@@ -12,40 +12,36 @@ public:
     PlayerGUI(PlayerAudio& audioPlayer);
     ~PlayerGUI() override = default;
 
-    // Component methods
     void paint(juce::Graphics& g) override;
     void resized() override;
-
-    // Event handlers
     void buttonClicked(juce::Button* button) override;
     void sliderValueChanged(juce::Slider* slider) override;
-
-    // Timer callback for real-time updates
     void timerCallback() override;
 
 private:
     PlayerAudio& playerAudio;
 
-    // GUI Controls
     juce::TextButton loadButton{ "Load File" };
     juce::TextButton playButton{ "Play" };
     juce::TextButton stopButton{ "Stop" };
     juce::TextButton loopButton{ "Loop" };
     juce::TextButton muteButton{ "Mute" };
 
-    // A-B Looping buttons
     juce::TextButton loopAButton{ "Set A" };
     juce::TextButton loopBButton{ "Set B" };
     juce::TextButton abLoopButton{ "A-B Loop" };
     juce::TextButton clearLoopButton{ "Clear" };
-    // Loop points labels
+
     juce::Label loopStartLabel;
     juce::Label loopEndLabel;
     juce::Label loopStatusLabel;
 
     juce::Slider volumeSlider;
+    juce::Label volumeLabel;
 
-    // Position slider and time displays
+    juce::Slider speedSlider;
+    juce::Label speedLabel;
+
     juce::Slider positionSlider;
     juce::Label currentTimeLabel;
     juce::Label durationLabel;
@@ -59,6 +55,7 @@ private:
     void updateABLoopButton();
     void updateLoopPointsDisplay();
     void updateTimeDisplays();
+    void updateSpeedDisplay();
     juce::String formatTime(double seconds);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PlayerGUI)
