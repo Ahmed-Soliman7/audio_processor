877ad28fe877532082fc5dce765b923d490ccd57
mohammedayman777 <ma75073377@gmail.com>
2025-11-03 00:45:49 +0200

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
 #include <JuceHeader.h>
 #include "MainComponent.h"
 
-// Our application class
 class SimpleAudioPlayer : public juce::JUCEApplication
 {
 public:
@@ -10,39 +9,27 @@ public:
 
     void initialise(const juce::String&) override
     {
-        // Create and show the main window
         mainWindow = std::make_unique<MainWindow>(getApplicationName());
     }
 
-    void shutdown() override
-    {
-        mainWindow = nullptr; // Clean up
-    }
+    void shutdown() override { mainWindow = nullptr; }
 
 private:
-    // The main window of the app
     class MainWindow : public juce::DocumentWindow
     {
     public:
-        MainWindow(juce::String name)
-            : DocumentWindow(name,
-                juce::Colours::lightgrey,
-                DocumentWindow::allButtons)
+        MainWindow(juce::String name) : DocumentWindow(name, juce::Colours::lightgrey, DocumentWindow::allButtons)
         {
             setUsingNativeTitleBar(true);
-            setContentOwned(new MainComponent(), true); // MainComponent = our UI + logic
-            centreWithSize(1220, 250);
+            setContentOwned(new MainComponent(), true);
+            centreWithSize(1220, 300);
             setVisible(true);
         }
 
-        void closeButtonPressed() override
-        {
-            juce::JUCEApplication::getInstance()->systemRequestedQuit();
-        }
+        void closeButtonPressed() override { juce::JUCEApplication::getInstance()->systemRequestedQuit(); }
     };
 
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-// This macro starts the app
 START_JUCE_APPLICATION(SimpleAudioPlayer)
